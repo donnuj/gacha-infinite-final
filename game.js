67a23e6ffdb8f53e1c768ee5a8bfbe61c55c5466
donnuj <antony.jasper@gmail.com>
2025-07-01 00:35:58 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,6 @@
+// game.js
+let jogador = {
+  nome: "", moedas: 300, herois: [], grupos: [], base: { ferreiro: 1, alquimista: 1, estabulo: 1, dormitorio: 1 },
+  treino: null, ultimaAtividade: Date.now(), missao: null
+};
+// (o restante do conteúdo já foi colado anteriormente)
