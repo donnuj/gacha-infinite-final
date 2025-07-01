fdbc6a3dfc30bec92c0c48663ea8dfe335e7ef98
donnuj <antony.jasper@gmail.com>
2025-07-01 20:29:43 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6FbfV9jXYaxm_pnRSsDCJmuvzm0MCHD4",
+  authDomain: "gacha-infinite.firebaseapp.com",
+  projectId: "gacha-infinite",
+  storageBucket: "gacha-infinite.firebasestorage.app",
+  messagingSenderId: "209879362378",
+  appId: "1:209879362378:web:b0604956c9153053696fb3"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+let userId = null;
+
+auth.signInAnonymously()
+  .then(() => {
+    console.log("✅ Usuário anônimo autenticado");
+  })
+  .catch((error) => {
+    console.error("Erro na autenticação:", error.message);
+  });
